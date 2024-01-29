d64c12569f4745da0da56a4cd9569e9d523fb96c
workwithraymond <workwithraymond01@gmail.com>
2024-01-29 06:44:22 -0500

    new changes to Gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,5 +1,6 @@
-import { useQuery } from 'react-query';
+import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
+import { useGlobalContext } from '../context';
 
 const url = 'https://api.unsplash.com/search/photos?client_id=CXdeBc15JY2dWKTt0qRjr-iiToStzC-3kVwVM4INQ-M&query=office'
 
@@ -11,14 +12,49 @@ const Gallery = () => {
     queryFn:async () => {
       const result = await axios.get(url)
 
-      return result.data
+      return result.data;
     }
   });
 
-  console.log(response);
+  if(response.isLoading) {
+    return (
+   <section className='image-container'>
+      <h4>Loading...</h4>
+    </section>   
+    )
+  }
+  if(response.isError) {
+    return(
+      <section className='image-container'>
+        <h4>There was an error...</h4>
+      </section>
+    )
+  }
+
+  const results = response.data.results 
+  if(results.length < 1) {
+    return(
+      <section className='image-container'>
+        <h4>No results found...</h4>
+      </section>
+    )
+  }
   
   return (
-    <div>Gallery</div>
+    <section className='image-container'>
+      {results.map((item) => {
+        const url = item?.urls?.regular
+        return (
+          <img 
+          src={url} 
+          key={item.id} 
+          alt={item.alt_description}
+          className='img'>
+
+          </img>
+        )
+      } )}
+      </section>
   )
 }
 export default Gallery
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { AppProvider } from './context';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <AppProvider>
