d32bdd1fb5801909730d948d5dd44b905ad21862
workwithraymond <workwithraymond01@gmail.com>
2024-01-30 06:56:13 -0500

    Changes to the gallery component

--- src/context.jsx
+++ src/context.jsx
@@ -2,8 +2,18 @@ import { createContext, useContext, useState, useEffect } from "react";
 
 const AppContext = createContext();
 
+const getInitialDarkMode = () => {
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme:dark)').matches
+    const storedDarkMode = localStorage.getItem('darkTheme');
+
+    if (storedDarkMode === null) {
+        return prefersDarkMode;
+    }
+    return storedDarkMode === 'true';
+}
+
 export const AppProvider = ({children}) => {
-    const [isDarkTheme, setIsDarkTheme] = useState(false);
+    const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
     const [searchTerm, setSearchTerm] = useState('cat');
 
 const toggleDarkTheme = () => {
@@ -11,7 +21,11 @@ const toggleDarkTheme = () => {
     setIsDarkTheme(newDarkTheme);
     const body = document.querySelector('body')
     body.classList.toggle('dark-theme', newDarkTheme);
-}
+};
+
+useEffect(() => {
+    document.body.classList.toggle('dark-theme', isDarkTheme)
+}, [])
     
     return(
         <AppContext.Provider value={{isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm}}>
