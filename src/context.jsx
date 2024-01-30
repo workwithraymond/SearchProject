7d299b56c684577332f76a5891f8037b43ef9cdf
workwithraymond <workwithraymond01@gmail.com>
2024-01-30 07:07:28 -0500

    made new changes

--- src/context.jsx
+++ src/context.jsx
@@ -19,13 +19,13 @@ export const AppProvider = ({children}) => {
 const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme
     setIsDarkTheme(newDarkTheme);
-    const body = document.querySelector('body')
-    body.classList.toggle('dark-theme', newDarkTheme);
+    localStorage.setItem('darkTheme', newDarkTheme);
+    
 };
 
 useEffect(() => {
     document.body.classList.toggle('dark-theme', isDarkTheme)
-}, [])
+}, [isDarkTheme])
     
     return(
         <AppContext.Provider value={{isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm}}>
